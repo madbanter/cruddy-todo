615cf20d909349061ffd0f72a123ce373b8ccad6
Daniel Wolfgang Malamud-Roam <dmalamudroam@gmail.com>
2020-12-15 10:44:40 -0800

    Delete is working

--- datastore/index.js
+++ datastore/index.js
@@ -47,10 +47,8 @@ exports.readAll = (callback) => {
       callback(null, data);
     }
   });
-  // callback(null, data);
-};
 
-// callback(new Error(`No item with id: ${id}`));
+};
 
 exports.readOne = (id, callback) => {
   var filePath = path.join(exports.dataDir, `${id}.txt`);
@@ -75,14 +73,14 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  var filePath = path.join(exports.dataDir, `${id}.txt`);
+  fs.rm(filePath, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
